1603bf47b604a28b5b52251bff00850c02465cf3
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-08-29 18:23:44 -0600

    Fixed issues with connecting to database

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
 export class MovieView extends React.Component {
+  
   render() {
     const { movie, onBackClick } = this.props;
 
     return ( 
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} />
+          <img crossOrigin="anonymous" width="200px" src={movie.imageURL} />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
+          <span className="value">{movie.title}</span>
         </div>
         <div className="movie-description">
           <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
+          <span className="value">{movie.description}</span>
         </div>
         <button onClick={() => {onBackClick(null);}}>Back</button>
       </div>
