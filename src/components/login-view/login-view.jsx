5160a94b5de5e6e14fbcf1dae630c347fc725dac
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-09-05 18:24:27 -0600

    Added form validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -8,37 +8,68 @@ import './login-view.scss';
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
+  // declare hooks for each input
+  const [ usernameErr, setUsernameErr ] = useState('');
+  const [ passwordErr, setPasswordErr ] = useState('');
+
+  // validate user inputs
+  const validate = () => {
+    let isReq = true;
+    if(!username){
+      setUsernameErr('Username required');
+      isReq = false;
+    } else if(username.length < 5) {
+      setUsernameErr('Username must be 5 characters long');
+      isReq = false;
+    }
+    if(!password){
+      setPasswordErr('Password required');
+      isReq = false;
+    } else if(password.length < 4){
+      setPasswordErr('Password must be 4 characters long');
+      isReq = false;
+    }
+
+    return isReq;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // send a request to server for authentication
-    axios.post('https://myflixbysarah.herokuapp.com/login', {
-      username: username,
-      password: password
-    })
-    .then(response => {
-      const data = response.data;
-      props.onLoggedIn(data);
-    })
-    .catch(e => {
-      console.log('no such user')
-    });
+    const isReq = validate();
+    if(isReq) {
+      // send a request to server for authentication
+      axios.post('https://myflixbysarah.herokuapp.com/login', {
+        username: username,
+        password: password
+      })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
+    }
   };
 
   return(
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
+    <div className="login-view">
+      <Form>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+          {usernameErr && <p>{usernameErr}</p>}
+        </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>Log In</Button>
-    </Form>
-  );
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} />
+          {passwordErr && <p>{passwordErr}</p>}
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>Log In</Button>
+      </Form>
+    </div>
+  )
 }
 
 LoginView.propTypes = {
