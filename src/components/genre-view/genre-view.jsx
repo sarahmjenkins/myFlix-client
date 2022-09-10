816eb26ab2cb36492a47c8171e61d3732163a155
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-09-10 14:39:12 -0600

    Refined genre view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -5,20 +5,35 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import './genre-view.scss';
 
+import { MovieCard } from '../movie-card/movie-card';
+
 export class GenreView extends React.Component {
+
   render () {
-    const { movie } = this.props;
+    const { genre, onBackClick } = this.props;
+
+    const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
 
     return (
       <Container>
         <Row className="genre-name justify-content-md-center">
-          <Col md={2} className="label">Genre:</Col>
-          <Col md={10} className="value">{movie.genre.name}</Col>
+          <Col md={8} className="header">{capitalize(genre.name)}</Col>
         </Row>
         <Row className="genre-description justify-content-md-center">
-          <Col md={2} className="label">Description:</Col>
-          <Col md={10} className="value">{movie.genre.description}</Col>
+          <Col md={8} className="genre-info">Description: {genre.description}</Col>
         </Row>
+        <Row className="genre-movies-header justify-content-md-center">
+          <Col md={8} className="sub-header">{capitalize(genre.name)} Movies</Col>
+        </Row>
+        {/* Tried to figure out a way to get GenreView to display MovieCard view for movies of that genre */}
+        {/* <Row>
+          <Col md={3}>  
+            {{movies.map((movie) => {
+              if (genre.name)
+              return <MovieCard key={movie._id} movie={movie} />
+            })} 
+          </Col> 
+        </Row> */}
         <Row className="justify-content-md-center">
           <Button onClick={() => {onBackClick();}}>Back</Button>
         </Row>
